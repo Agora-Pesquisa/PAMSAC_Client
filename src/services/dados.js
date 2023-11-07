0e1548d4d1e3c61929ecd606ed04c5e3e73596f9
agorapesquisa <138153270+agorapesquisa@users.noreply.github.com>
2023-11-07 16:18:03 -0300

    Update dados.js

--- src/services/dados.js
+++ src/services/dados.js
@@ -3,7 +3,7 @@
 /* cd C:\Codes\plataforma-aeroporto\client */
 /* cd C:\Codes\plataforma-aeroporto\server */
 
-const url = "https://19ff-20-168-42-226.ngrok-free.app";
+const url = "http://20.168.42.226:7000/";
 
 /* Função que pega as informações dos parametros de Login e senha digitados pelo client e
 faz uma requisição para a API com esses parametros*/
