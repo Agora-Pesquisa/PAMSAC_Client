ca15fe3e1dc90618e73daf3a383f7757bbf22a24
agorapesquisa <138153270+agorapesquisa@users.noreply.github.com>
2024-01-23 15:40:37 -0300

    Update Header.jsx

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,33 +1,27 @@
-import React from "react"; // Importação do React
-import User from "./User/User"; // Importação do componente User
-import UserCircle from "./UserCircle/UserCircle"; // Importação do componente UserCircle
-import styles from "./Header.module.css"; // Importação do módulo de estilos específico para o componente
+import React from "react"; 
+import User from "./User/User"; 
+import UserCircle from "./UserCircle/UserCircle"; 
+import styles from "./Header.module.css"; 
 
-// Componente funcional para o cabeçalho
 const Header = (props) => {
-  // Função para lidar com o clique nos elementos do cabeçalho
   const handleClick = (e) => {
-    props.selecionarUser(e.target.id); // Chama a função passada via propriedade para atualizar o usuário selecionado
+    props.selecionarUser(e.target.id);
   };
 
-  // Renderização do componente de cabeçalho
   return (
     <div className={styles.Master}>
       <div
-        id={props.userSelecionado} // Define o ID do elemento com base no usuário selecionado
-        onClick={handleClick} // Define a função de clique para o elemento
+        id={props.userSelecionado} 
+        onClick={handleClick} 
         className={`${styles.ContainerMasterHeader} , ${
           props.userSelecionado === "Bem-vindo!" ? styles.Loading : ""
-        }`} // Aplica a classe de estilo ao contêiner do cabeçalho
+        }`} 
       >
-        {/* Componente User para exibir o nome do usuário */}
         <User user={props.userSelecionado} />
-        {/* Componente UserCircle para exibir o círculo do usuário */}
         <UserCircle user={props.userSelecionado} />
       </div>
     </div>
   );
 };
 
-// Exportação do componente Header
 export default Header;
