a30fcd861afef1367d038de4decb31f4d0ba2337
agorapesquisa <138153270+agorapesquisa@users.noreply.github.com>
2024-01-23 15:41:57 -0300

    Update UserCircle.jsx

--- src/components/Home/Header/UserCircle/UserCircle.jsx
+++ src/components/Home/Header/UserCircle/UserCircle.jsx
@@ -1,29 +1,11 @@
-import React from "react"; // Importação do React
-import styles from "./UserCircle.module.css"; // Importação do módulo de estilos específico para o componente
+import React from "react"; 
+import styles from "./UserCircle.module.css";
 
-// Componente funcional para exibir o círculo do usuário
 const UserCircle = (props) => {
-  // Renderização do componente UserCircle
   return (
     <div>
-      {/* Retirar essa DIV quando  */}
     </div>
-    /*
-    <div
-     
-      id={props.user} // Define o ID do elemento com base no usuário recebido via propriedade
-      className={styles.ContainerMasterUserCircle} // Aplica a classe de estilo ao contêiner do círculo do usuário
-    >
-      <div
-        id={props.user} // Define o ID do elemento com base no usuário recebido via propriedade
-        className={styles.User} // Aplica a classe de estilo ao elemento de texto do usuário
-      >
-      {props.user} Exibe o nome do usuário 
-      </div>
-    </div>
-    */
   );
 };
 
-// Exportação do componente UserCircle
 export default UserCircle;
