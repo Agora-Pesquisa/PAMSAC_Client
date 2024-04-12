0191ba496d81159834764dc03e0e8cf8d2650813
Caio Santos Martins <agorapesquisa.dados@gmail.com>
2024-04-12 09:21:35 -0300

    atualizando api

--- src/services/dados.js
+++ src/services/dados.js
@@ -3,12 +3,12 @@
 /* cd C:\Codes\plataforma-aeroporto\client */
 /* cd C:\Codes\plataforma-aeroporto\server */
 
-const url = "https://19ff-20-168-42-226.ngrok-free.app";
-/*teste*/
+const url = "https://14c0-20-168-42-226.ngrok-free.app";
+
 /* Função que pega as informações dos parametros de Login e senha digitados pelo client e
 faz uma requisição para a API com esses parametros*/
 async function getDados(login, senha) {
-  const data = await fetch(`${url}/${login}/${senha}`, {
+  const data = await fetch(`${url}/PAMSAC/login${login}/${senha}`, {
     method: "get",
     headers: new Headers({
       "ngrok-skip-browser-warning": "69420",
