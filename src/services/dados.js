d0b801b4914d72979d04559bf3cfad12fed09a06
agorapesquisa <138153270+agorapesquisa@users.noreply.github.com>
2024-11-13 11:55:24 -0300

    Update dados.js

--- src/services/dados.js
+++ src/services/dados.js
@@ -1,4 +1,4 @@
-const url = "https://6c52-20-168-42-226.ngrok-free.app";
+const url = "https://cb44-20-168-42-226.ngrok-free.app";
 
 async function getDados(login, senha) {
   const data = await fetch(`${url}/PAMSAC/login/${login}/${senha}`, {
